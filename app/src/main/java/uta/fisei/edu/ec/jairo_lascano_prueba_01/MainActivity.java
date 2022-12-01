8952df004d2510f15937b8b542f798af852a227e
AndyMntr <85909503+AndyMntr@users.noreply.github.com>
2022-12-01 18:15:48 -0500

    hora 18:15

--- app/src/main/java/uta/fisei/edu/ec/jairo_lascano_prueba_01/MainActivity.java
+++ app/src/main/java/uta/fisei/edu/ec/jairo_lascano_prueba_01/MainActivity.java
@@ -3,12 +3,33 @@ package uta.fisei.edu.ec.jairo_lascano_prueba_01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button buttonSiguiente;
+    private Button buttonMostrarResultado;
+    private EditText editTextNombres;
+    private EditText editTextApellidos;
+    private EditText editTextDividendo;
+    private EditText editTextDivisor;
+    private EditText editTextParteEntera;
+    private EditText editTextResiduo;
+    private EditText editTextNumInvertido;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonMostrarResultado = findViewById(R.id.buttonResul);
+        buttonSiguiente = findViewById(R.id.buttonSiguiente);
+        editTextNombres = findViewById(R.id.editTextTextNombres);
+        editTextApellidos = findViewById(R.id.editTextApellidos);
+        editTextDividendo = findViewById(R.id.editTextDividendo);
+        editTextDivisor = findViewById(R.id.editTextDivisor);
+        editTextParteEntera = findViewById(R.id.editTextParteEntera);
+        editTextNumInvertido = findViewById(R.id.editTextNumero);
+        editTextResiduo = findViewById(R.id.editTextResiduo);
+
     }
 }
