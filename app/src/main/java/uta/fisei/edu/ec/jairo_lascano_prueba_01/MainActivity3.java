cf75a84c81a84ff1624a088bc629b12c6569cc72
AndyMntr <85909503+AndyMntr@users.noreply.github.com>
2022-12-01 19:14:31 -0500

    hora 19:14

--- app/src/main/java/uta/fisei/edu/ec/jairo_lascano_prueba_01/MainActivity3.java
+++ app/src/main/java/uta/fisei/edu/ec/jairo_lascano_prueba_01/MainActivity3.java
@@ -2,15 +2,21 @@ package uta.fisei.edu.ec.jairo_lascano_prueba_01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity3 extends AppCompatActivity {
 
+    private Button buttoncerrar;
     private EditText editTextNombres;
     private EditText editTextApellidos;
-    private TextView textViewNombres;
+    private EditText editTextDividendo;
+    private EditText editTextDivisor;
+    private EditText editTextNumero;
 
 
     @Override
@@ -18,17 +24,36 @@ public class MainActivity3 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
+        buttoncerrar = findViewById(R.id.buttuonCerrar);
         editTextNombres = findViewById(R.id.editTextTextNombres);
         editTextApellidos = findViewById(R.id.editTextApellidos);
-        textViewNombres = findViewById(R.id.textViewNombres);
+        editTextDividendo = findViewById(R.id.editTextDividendo);
+        editTextDivisor = findViewById(R.id.editTextDivisor);
+        editTextNumero = findViewById(R.id.editTextNumero);
+
 
         Bundle datos = this.getIntent().getExtras();
-        String Nombres = datos.getString("Nombres");
-        String Apellidos = datos.getString("Apellidos");
 
-        textViewNombres.setText(Nombres);
+        if(datos !=null){
+            String nombresCap = datos.getString("Nombres");
+            String Apellidos = datos.getString("Apellidos");
+            editTextNombres.setText(nombresCap);
+            editTextApellidos.setText(Apellidos);
+        }
 
-        editTextNombres.setText(Nombres);
-        editTextApellidos.setText(Apellidos);
+
+    }
+
+    public void  Cerrar (View view){
+
+        Intent intent = new Intent (this, MainActivity.class);
+        intent.putExtra("Nombres",editTextNombres.getText());
+        intent.putExtra("Apellidos", editTextApellidos.getText());
+        intent.putExtra("Divideno",editTextDividendo.getText());
+        intent.putExtra("Divosor",editTextDivisor.getText());
+        intent.putExtra("Numero",editTextNumero.getText());
+        startActivity(intent);
+
+        finish();
     }
 }
